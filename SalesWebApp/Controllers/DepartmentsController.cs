955dfbc196d54cd3152053ff9fe8ee2c558c72b4
TRBARROS <thiago_NO1@outlook.com>
2023-05-05 22:36:10 -0300

    Start

---
+++ SalesWebApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebApp.Models;
+
+namespace SalesWebApp.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> List = new List<Departament>();
+            List.Add(new Departament { Id = 1, Name = "Eletronics"});
+            List.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(List);
+        }
+    }
+}
--- SalesWebApp/Controllers/HomeController.cs
+++ SalesWebApp/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebApp.Models;
+using SalesWebApp.Models.ViewModels;
 using System.Diagnostics;
 
 namespace SalesWebApp.Controllers
